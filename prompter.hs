1a9148f566a7eaaed5f5e4cd8bff8bdb13e84de1
Raveline <root@sd-15896.dedibox.fr>
2013-02-26 17:02:32 +0100

    Just toying with the prompter, nothing serious (and, most likely, nothing that will stay there)

--- prompter.hs
+++ prompter.hs
@@ -1,14 +1,43 @@
+import Data.List
+import Data.Char
+
 data PlayerAction = Quit | Action String 
+data StatementStyle = Polite | Vulgar | Neutral
+
+vulgarity = ["fuck", "shit", "cunt", "wtf"]
+politeness = ["please", "hello", "salutations", "would you mind"]
+quit = ["good bye", "quit", "ciao", "see you", "bye bye", "sayonara", "arrivederci"]
+vulgarity_reaction = "I can't stand such vulgarity. Please behave !"
+politeness_reaction = "How polite of you."
+
 
 parseLine :: String -> PlayerAction
-parseLine "quit" = Quit
-parseLine s = Action s
+parseLine s
+    | s `elem` quit  = Quit
+    | otherwise = Action s
 
 proceed :: PlayerAction -> IO()
 proceed (Action s) = 
     do 
-        putStrLn $ "You said : " ++ s
+        putStrLn $ reactToBasicAction (map toLower s)
         promptLoop
+
+reactToBasicAction :: String -> String
+reactToBasicAction s = interpret $ getStatementStyle s
+    where 
+        interpret :: StatementStyle -> String
+        interpret Polite = politeness_reaction 
+        interpret Vulgar = vulgarity_reaction 
+        interpret Neutral = "You said : " ++ s
+
+getStatementStyle :: String -> StatementStyle
+getStatementStyle s 
+        | containsWords s vulgarity = Vulgar
+        | containsWords s politeness = Polite
+        | otherwise = Neutral
+
+containsWords :: String -> [String] -> Bool
+containsWords s ws = intersect (words s) ws /= []
 
 isQuit :: PlayerAction -> Bool
 isQuit Quit = True
