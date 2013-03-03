d2698605d944747be67dbdf0204dc3bdb407c84c
Raveline <raveline@sd-15896.dedibox.fr>
2013-03-03 18:53:54 +0100

    Updated spec. Those show the Parser is failing for now.

--- SpecAction.hs
+++ SpecAction.hs
@@ -2,10 +2,11 @@
 import Test.Hspec
 import Test.QuickCheck
 import Action
+import Parser
 
-verbExample = Transitive "speak" ["with", "to"]
-verbExample' = Transitive "ask" ["about"]
-verbExample'' = Phrasal "look" "for" []
+verbExample = Transitive Talk "speak" ["with", "to"] ["about"]
+verbExample' = Transitive Talk "ask" ["about"] []
+verbExample'' = Phrasal Search "look" "for" [] ["in", "with"]
 verbs = [verbExample, verbExample', verbExample'']
 
 
@@ -13,10 +14,10 @@ main :: IO()
 main = hspec $ do
     describe "processInput" $ do
             it "Finds the transitive verb Speak to in a sentence" $ do
-                processInput "Speak to the neighbour" verbs `shouldBe` Just(verbExample)
+                processInput "Speak to the neighbour" verbs `shouldBe` Interaction Talk (Object "the neighbour")
 
             it "Doesn't find a verb in a sentence" $ do
-                processInput "Behold the power of the specs" verbs `shouldBe` Nothing
+                processInput "Behold the power of the specs" verbs `shouldBe` SimpleAction Zilch
 
             it "Finds the phrasal verb \"Look for\" in a sentence" $ do
-                processInput "Look for an answer to this riddle" verbs `shouldBe` Just(verbExample'')
+                processInput "Look for an answer to this riddle" verbs `shouldBe` Interaction Search (Object "an answer to this riddle")
