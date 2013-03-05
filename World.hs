c160118ca61338842a0b16aae08e89dd42a58feb
Raveline <raveline@raveline-RC530-RC730.(none)>
2013-03-06 00:03:23 +0100

    Added some Show, put the constant outside of the file and into Lambdacula.

--- World.hs
+++ World.hs
@@ -12,19 +12,14 @@ import Action
 type Conversations = Map.Map String String
 type Interactions = Map.Map Action String
 
-testCube = RoomObject "Test cube" (Map.fromList[(Examine, "A simple test cube. Look, how pretty !"),
-                                            (Talk, "You can't talk to a cube, don't be silly"),
-                                            (Move, "You push the cube. Happy now ?")]) 
-
-room = Room "The test room" "You are standing in a non-existant place in a virtual world. This is a very good place to hold existential thoughts. Or test the system. But this is more or less the same thing, innit ?" [testCube] [] [] 
-
 data Room =    Room { roomName :: String
                  ,description :: String
                  ,objects :: [RoomObject]
                  ,characters :: [Character]
                  ,exit :: [Exit]
                 }
+    deriving (Show)
 
-data RoomObject = RoomObject { objectName :: String, objectReactions :: Interactions }
-data Exit = Exit { exitDescription :: String, destination :: String }
-data Character = Character {name :: String, aliases :: [String], topics :: Conversations, npcReactions :: Interactions }
+data RoomObject = RoomObject { objectName :: String, objectReactions :: Interactions } deriving (Show)
+data Exit = Exit { exitDescription :: String, destination :: String } deriving (Show)
+data Character = Character {name :: String, aliases :: [String], topics :: Conversations, npcReactions :: Interactions } deriving (Show)
