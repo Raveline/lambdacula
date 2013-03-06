2cadfd0106e54540b2630768a92377f9d593bf56
Raveline <raveline@raveline-RC530-RC730.(none)>
2013-03-07 00:01:26 +0100

    Add to adapt the tests to the disparition of the Object type.

--- SpecAction.hs
+++ SpecAction.hs
@@ -16,13 +16,13 @@ main :: IO()
 main = hspec $ do
     describe "processInput" $ do
             it "Finds the transitive verb Speak to in a sentence" $ do
-                processInput "Speak to the neighbour" verbs `shouldBe` Interaction Talk (Object "neighbour")
+                processInput "Speak to the neighbour" verbs `shouldBe` Interaction Talk "neighbour"
 
             it "Doesn't find a verb in a sentence" $ do
                 processInput "Behold the power of the specs" verbs `shouldBe` SimpleAction Zilch
 
             it "Finds the phrasal verb \"Look for\" in a sentence" $ do
-                processInput "Look for an answer to this riddle" verbs `shouldBe` Interaction Search (Object "answer to this riddle")
+                processInput "Look for an answer to this riddle" verbs `shouldBe` Interaction Search "answer to this riddle"
 
             it "Finds a complex action with two objets" $ do
-                processInput "Speak with the count about the meaning of life" verbs `shouldBe` Complex Talk (Object "count") (Object "meaning of life")
+                processInput "Speak with the count about the meaning of life" verbs `shouldBe` Complex Talk "count" "meaning of life"
