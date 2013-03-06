64f086182846b027bd1dbdcda9aa75497096d98d
Raveline <raveline@raveline-RC530-RC730.(none)>
2013-03-07 00:02:33 +0100

    Improved a little bit, but no display when actions are performed. Check why.

--- Lambdacula.hs
+++ Lambdacula.hs
@@ -10,10 +10,20 @@ isQuit (SimpleAction QuitGame) = True
 isQuit _ = False
 
 proceed :: PlayerAction -> World -> IO ()
-proceed (SimpleAction Zilch) p = do
+proceed (SimpleAction Zilch) w = do
                                 putStrLn "Huh ?"
-                                promptLoop p
+                                promptLoop w
+proceed (Interaction t o) w = 
+                        do
+                            x <- return $ findObject o (currentRoom w)
+                            case x of
+                                Nothing -> putStrLn $ "There is no " ++ o ++ " here !"
+                                Just realObject -> putStrLn (actionOn t realObject)
+                            promptLoop w
 proceed _ p = promptLoop p
+
+actionOn :: Action -> RoomObject -> String
+actionOn act obj = getTextForAction act obj
 
 promptLoop :: World -> IO ()
 promptLoop player = do
@@ -32,6 +42,11 @@ displayRoom (Room name desc _ _ _) = do
                                 putStrLn $ replicate (length name) '*' 
                                 putStrLn desc
 
+findObject :: String -> Room -> Maybe RoomObject
+findObject s room = find (isObject s) (objects room)
+    where   isObject :: String -> RoomObject -> Bool
+            isObject s o = s `elem` (objectAliases o)
+
 main = do
         displayRoom $ currentRoom aWorld
         promptLoop aWorld
@@ -40,15 +55,20 @@ aWorld = World (Player []) room
 
 -- here are some constants to test this code.
 speak = Transitive Talk "speak" ["with", "to"] ["about"]
+talk = Transitive Talk "talk" ["with", "to"] ["about"]
 ask = Transitive Talk "ask" ["about"] []
 lookFor = Phrasal Search "look" "for" [] ["in", "with"]
+examine = Transitive Examine "examine" [] ["with"]
+look = Transitive Examine "look" [] ["with"]
+analyze = Transitive Examine "analyze" [] []
 quit = Transitive QuitGame "quit" [] []
-verbs = [speak, ask, lookFor, quit]
+verbs = [speak, talk, ask, lookFor, examine, look, analyze, quit]
 
 
-testCube = RoomObject "Test cube" (Map.fromList[(Examine, "A simple test cube. Look, how pretty !"),
+testCube = RoomObject "the test cube" ["Test cube", "cube"] (Map.fromList[(Examine, "A simple test cube. Look, how pretty !"),
                                             (Talk, "You can't talk to a cube, don't be silly"),
                                             (Move, "You push the cube. Happy now ?")]) 
+
 
 room = Room "The test room" "You are standing in a non-existant place in a virtual world. This is a very good place to hold existential thoughts. Or test the system. But this is more or less the same thing, innit ?" [testCube] [] [] 
 
