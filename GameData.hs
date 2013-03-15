ee6d6dd71e30f9f95197097573fb1c85baa5bed7
raveline <eraveline@gmail.com>
2013-03-16 00:59:11 +0100

    Corrected exits (pointing to the same room).

--- GameData.hs
+++ GameData.hs
@@ -14,7 +14,8 @@ examine = Transitive Examine "examine" [] ["with"]
 lookAt = Phrasal Examine "look" "at" [] ["with"]
 look = Transitive Examine "look" [] ["with"] 
 analyze = Transitive Examine "analyze" [] []
-go = Transitive Talk "go" [] []
+go = Transitive Move "go" [] []
+eat = Transitive Eat "eat" [] []
 quit = Transitive QuitGame "quit" [] []
 
 -- OBJECTS
@@ -25,13 +26,14 @@ useTestCube Examine = singleAnswer "A simple test cube. Look, how pretty !"
 useTestCube Talk = singleAnswer "You can't talk to a cube, don't be silly."
 useTestCube Move = singleAnswer "You push the cube. Happy now ?"
 useTestCube Open = singleAnswer "You don't find any opening on the cube"
+useTestCube Eat = singleAnswer "You try to eat the cube. It's not very good. Particularly for your teeth."
 useTestCube _ = singleAnswer "You can't do that to the test cube" -- TO CHANGE. Return empty string, and deal with this input in proceed. 
 
 
-verbs = [speak, talk, ask, lookFor, lookAt, examine, look, analyze, go, quit]
+verbs = [speak, talk, ask, lookFor, lookAt, examine, look, analyze, go, eat, quit]
 
-fromOneToTwo = Exit "north" [] "a weird discontinuity in space and time" (basicMove room) True 
-fromTwoToOne = Exit "south" [] "a passage that defies the law of physics" (basicMove room') True 
+fromOneToTwo = Exit "north" [] "a weird discontinuity in space and time" (basicMove room') True 
+fromTwoToOne = Exit "south" [] "a passage that defies the law of physics" (basicMove room) True 
 
 room = Room "The test room" "You are standing in a non-existant place in a virtual world. This is a very good place to hold existential thoughts. Or test the system. But this is more or less the same thing, innit ?\nThere is a nice **test cube** in the center of the non-space." [testCube] [] [fromOneToTwo] 
 room' = Room "A second room" "You are in a second room. It doesn't exist, like the first one; so really, you moved but you didn't move. I know, I know, this sounds absurd. And to a point, it is." [] [] [fromTwoToOne]
