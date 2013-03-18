ebd7be92acf45389e25bea8bf83cfd1fa6ff58c1
raveline <eraveline@gmail.com>
2013-03-18 20:00:26 +0100

    Err. Not sure this is really useful.

---
+++ src/Lambdacula.hs
@@ -0,0 +1,13 @@
+module Lambdacula (
+    module Lambdacula.Action,
+    module Lambdacula.GameData,
+    module Lambdacula.World,
+    module Lambdacula.Parser,
+    module Lambdacula.Display
+) where
+
+import Lambdacula.Action
+import Lambdacula.GameData
+import Lambdacula.World
+import Lambdacula.Parser
+import Lambdacula.Display
