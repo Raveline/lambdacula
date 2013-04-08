4d97ab104a04825ae04d96ea20718b1af609fe73
raveline <eraveline@gmail.com>
2013-04-08 19:44:51 +0200

    Inventory management and room object management functions.

--- src/Lambdacula/World.hs
+++ src/Lambdacula/World.hs
@@ -21,7 +21,7 @@ module Lambdacula.World
     updateCurrentRoom,
     updateRoomObjects,
     updateObjectStatus,
-    displayContainerContent
+    pickItem
 )
 where
 
@@ -39,6 +39,10 @@ type Conversations = Map.Map String String
 type Interactions = Map.Map Action String
 type WorldAction = State World [String]
 
+-- *************
+-- HERE BE TYPES
+-- *************
+
 class Actionable f where 
     -- Process an action on this object, return the world 
     -- and a string to tell what happened
@@ -47,6 +51,8 @@ class Actionable f where
     -- at aliases).
     match :: String -> f -> Bool
 
+-- A room. A piece of the universe if the universe was only carved
+-- out of rooms. Which would be handy for creators of zork-like games.
 data Room =    Room { _roomName :: String
                  ,_description :: String
                  ,_objects :: [RoomObject]
@@ -55,22 +61,39 @@ data Room =    Room { _roomName :: String
                 }
   deriving (Show, Eq)
 
+-- Room lenses
 objects :: Simple Lens Room [RoomObject]
-objects = lens _objects (\r objs ->  Room (_roomName r) (_description r) objs (_characters r) (_exits r))
+objects = lens _objects (\r objs ->  r {_objects = objs})
 
+-- The world. A scary place. It figures the encounters between a hero,
+-- the Player, and one of many room (the rooms) in a CurrentRoom.
 data World = World { _player :: Player, 
                     _currentRoom :: Room, 
                     _worldRooms :: [Room] }
 
+-- World lenses
 worldRooms :: Simple Lens World [Room]
-worldRooms = lens _worldRooms (\w rs -> World (_player w) (_currentRoom w) rs)
+worldRooms = lens _worldRooms (\w rs -> w {_worldRooms = rs})
 currentRoom :: Simple Lens World Room
-currentRoom = lens _currentRoom (\w cr -> World (_player w) cr (_worldRooms w))
+currentRoom = lens _currentRoom (\w cr -> w {_currentRoom = cr})
 
-
-data Player = Player { inventory :: [String] }
+-- A player. A hero. Currently, only there has a quite capitalistic
+-- representation of an inventory.
+data Player = Player { _inventory :: [String] }
     deriving (Show)
 
+-- Player lenses
+inventory :: Simple Lens Player [String]
+inventory = lens _inventory (\p inv -> p {_inventory = inv})
+
+-- A room object. A thing that stays in a room, waiting to be acted upon,
+-- or acting as a simple decoy.
+-- It has a main name for identification, and potential aliases for one same
+-- thing tends to have different names.
+-- It stores the way its react to actions through a method.
+-- It can contains other objects.
+-- It finally can have a Status. Changing objects status is more or less the
+-- way to navigate through the game.
 data RoomObject =    RoomObject { 
                      _objectName :: String
                     ,_objectAliases :: [String]
@@ -78,18 +101,21 @@ data RoomObject =    RoomObject {
                     ,_objectInside :: [RoomObject]
                     ,_objectStatus :: ObjectStatus
                     }
+
+-- RoomObject lenses
 objectStatus :: Simple Lens RoomObject ObjectStatus
 objectStatus = lens _objectStatus (\ro s -> ro {_objectStatus = s})
 
 objectName :: Simple Lens RoomObject String
-objectName = lens _objectName (\ro s -> RoomObject s (_objectAliases ro) (_objectReactions ro) (_objectInside ro) (_objectStatus ro))
+objectName = lens _objectName (\ro s -> ro {_objectName = s}) 
 
 objectAliases :: Simple Lens RoomObject [String]
-objectAliases = lens _objectAliases (\ro als -> RoomObject (_objectName ro) als (_objectReactions ro) (_objectInside ro) (_objectStatus ro))
+objectAliases = lens _objectAliases (\ro als -> ro {_objectAliases = als})
 
 objectReactions :: Simple Lens RoomObject (RoomObject -> Action -> WorldAction)
-objectReactions = lens _objectReactions (\ro rea -> RoomObject (_objectName ro) (_objectAliases ro) rea (_objectInside ro) (_objectStatus ro))
+objectReactions = lens _objectReactions (\ro rea -> ro {_objectReactions = rea}) 
 
+-- Room object instances
 instance Show RoomObject where
     show = show . view objectName
 
@@ -100,6 +126,9 @@ instance Actionable RoomObject where
     actOn r = view objectReactions r r
     match s = (s `elem`) . ((:) <$> view objectName <*> view objectAliases)
 
+-- An exit. Actually, currently, I'm really wondering if this particular
+-- type is necessary. For an exit is a RoomObject, if you think about it.
+-- I might want to make another constructor for RoomObject just for this.
 data Exit = Exit { _exitName :: String
                 , _exitAliases :: [String] 
                 , _exitDescription :: String
@@ -128,6 +157,9 @@ instance Actionable Exit where
     actOn e = (view exitActions e) e 
     match s = (s `elem`) . ((:) <$> _exitName <*> _exitAliases)
 
+-- A character. Someone that should be able to converse with a player. One day.
+-- I've got the same issue with characters and exits : shouldn't they be
+-- RoomObject, really ?
 data Character = Character {    name :: String, 
                                 aliases :: [String], 
                                 topics :: Conversations, 
@@ -141,6 +173,9 @@ instance Actionable Character where
 data ObjectStatus = Opened | Closed | Nada
 
 
+-- *****************
+-- UTILITY FUNCTIONS
+-- *****************
 
 -- Given a string and a room, try to find a RoomObject,
 -- an Exit or a Character matching the string. Send back
@@ -181,6 +216,17 @@ rebuildList (x:xs) old new
     | x == old = new:(rebuildList xs old new)
     | otherwise = x:(rebuildList xs old new)  
 
--- Display the objects contained by a container
-displayContainerContent :: RoomObject -> [String] 
-displayContainerContent (RoomObject _ _ _ contained _) = [_objectName x| x <- contained]
+-- Add an item to the player inventory. 
+-- Note that the item thusly taken must be "erased" from where it
+-- was taken by the caller. 
+pickItem :: World -> RoomObject -> Player 
+pickItem world ro = player' & inventory .~ (_objectName ro:(player'^.inventory))
+                where player' = _player world
+
+removeFromRoom :: RoomObject -> WorldAction
+removeFromRoom ro = do
+                        w <- get
+                        current <- use currentRoom
+                        let newRoom = current & objects .~ filter (== ro) (current^.objects)
+                        put (updateCurrentRoom w newRoom)
+                        return []
