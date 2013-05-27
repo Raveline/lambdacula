35b3fc00e6f3e2822852467ac7863d35ef6b9d4d
raveline <eraveline@gmail.com>
2013-05-27 19:44:48 +0200

    Fixed imports.

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,10 @@ where
 
 import Lambdacula.GameData
 import Lambdacula.WorldBuilder
+import Lambdacula.Display
+import Lambdacula.World
+import Lambdacula.Flow
+import Control.Lens
 
 main = do
         printStrs $ displayRoom (view currentRoom aWorld) (view currentObjects aWorld)
