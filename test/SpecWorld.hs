8d89badb63f60e35ea2a1b54231da2672521ba72
raveline <eraveline@gmail.com>
2013-05-28 00:50:48 +0200

    Added test for complex action.

--- test/SpecWorld.hs
+++ test/SpecWorld.hs
@@ -29,16 +29,18 @@ import qualified Data.Map as Map
 examineString = "A simple test cube. Look, how pretty !" 
 zilchString = "You can't do that to the test cube" 
 noSuchObjectString = "There is no gizmo here !"
+cubeWithWeather = "The cube has nothing to say about the weather."
 
 -- Test method for a test cube
-useTestCube :: RoomObject -> Action -> WorldAction 
-useTestCube cube Examine = singleAnswer examineString
-useTestCube _ Talk = singleAnswer "You can't talk to a cube, don't be silly."
-useTestCube _ Move = singleAnswer "You push the cube. Happy now ?"
-useTestCube cube Open = openContainer cube "You open the cube !"
-useTestCube _ Eat = singleAnswer "You try to eat the cube. It's not very good. Particularly for your teeth."
-useTestCube cube Take = pickItem cube
-useTestCube _ _ = singleAnswer zilchString 
+useTestCube :: RoomObject -> Action -> Maybe String -> WorldAction 
+useTestCube cube Talk (Just "weather") = singleAnswer cubeWithWeather
+useTestCube cube Examine _ = singleAnswer examineString
+useTestCube _ Talk _ = singleAnswer "You can't talk to a cube, don't be silly."
+useTestCube _ Move _ = singleAnswer "You push the cube. Happy now ?"
+useTestCube cube Open _ = openContainer cube "You open the cube !"
+useTestCube _ Eat _ = singleAnswer "You try to eat the cube. It's not very good. Particularly for your teeth."
+useTestCube cube Take _ = pickItem cube
+useTestCube _ _ _ = singleAnswer zilchString 
 
 trRooms = [Room "The test room" "You are standing in a non-existant place in a virtual world. This is a very good place to hold existential thoughts. Or test the system. But this is more or less the same thing, innit ?" 
             , Room "A second room" "You are in a second room. It doesn't exist, like the first one; so really, you moved but you didn't move. I know, I know, this sounds absurd. And to a point, it is."] 
@@ -47,8 +49,8 @@ trObjects = [makeExit "north" [] "The test room" "a weird discontinuity in space
             , makeExit "south" [] "A second room" "a passage that defies the law of physics" "The test room" Opened
             , RoomObject (ObjectNames ["the test cube","test cube", "cube"]) "The test room" (useTestCube) (RoomObjectDetails Closed "There is a nice test cube here." [basicObject])]
 
-noReaction :: RoomObject -> Action -> WorldAction
-noReaction _ _ = singleAnswer "This object is just for tests."
+noReaction :: RoomObject -> Action -> Maybe String -> WorldAction
+noReaction _ _ _ = singleAnswer "This object is just for tests."
 
 basicObject = simpleObject ["a thingy", "thingy"] "NOWHERE" noReaction "Nothing worth looking at"
 
@@ -66,3 +68,6 @@ main = hspec $ do
 
             it "Doesn't find an object and says so" $ do
                 testProceed (Interaction Open "gizmo") `shouldBe` noSuchObjectString 
+
+            it "Handle an interaction" $ do
+                testProceed (Complex Talk "cube" "weather") `shouldBe` cubeWithWeather
