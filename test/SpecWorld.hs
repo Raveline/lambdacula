bcf2786612fdd0b495981576da609a94a3d8f70b
raveline <eraveline@gmail.com>
2013-05-29 18:12:00 +0200

    Fixed after refactoring. 40% of behaviour testing done.

--- test/SpecWorld.hs
+++ test/SpecWorld.hs
@@ -24,6 +24,8 @@ import Lambdacula.World
 import Lambdacula.GameData
 import Lambdacula.WorldBuilder
 import Lambdacula.Flow
+import Lambdacula.ModelShortcuts
+import Lambdacula.Display
 import qualified Data.Map as Map
 import Data.List
 
@@ -45,11 +47,20 @@ useTestCube _ Eat _ = singleAnswer "You try to eat the cube. It's not very good.
 useTestCube cube Take _ = pickItem cube
 useTestCube _ _ _ = singleAnswer zilchString 
 
+-- Test method for the door
+useTestDoor :: MoveAction
+useTestDoor dest door Move nthg = basicMove dest door Move nthg
+useTestDoor _ door Use (Just "key")
+    | door^.objectStatus == Closed = do
+                                    changeStatus door Opened
+                                    return ["You open the door"]
+    | otherwise = singleAnswer $ "The door is already opened !"
+
 trRooms = [Room "The test room" "You are standing in a non-existant place in a virtual world. This is a very good place to hold existential thoughts. Or test the system. But this is more or less the same thing, innit ?" 
             , Room "A second room" "You are in a second room. It doesn't exist, like the first one; so really, you moved but you didn't move. I know, I know, this sounds absurd. And to a point, it is."] 
 
-trObjects = [makeExit "north" [] "The test room" "a weird discontinuity in space and time" "A second room" Closed 
-            , makeExit "south" [] "A second room" "a passage that defies the law of physics" "The test room" Opened
+trObjects = [Exit (ObjectNames ["A test door", "door"]) "The test room" (useTestDoor "The test room") (RoomObjectDetails Closed "A hermetically locked door" []) "A second room" 
+            , makeExit "south" [] "A second room" "A door" "The test room" Opened
             , RoomObject (ObjectNames ["the test cube","test cube", "cube"]) "The test room" (useTestCube) (RoomObjectDetails Closed "There is a nice test cube here." [basicObject])]
 
 noReaction :: RoomObject -> Action -> Maybe String -> WorldAction
@@ -84,8 +95,9 @@ checkStatus s st w = case find ((==) s . mainName) (_worldObjects w) of
 checkCurrentRoom :: String -> World -> Bool
 checkCurrentRoom s w = (_roomName . _currentRoom $ w) == s
 
-properActions = [(Interaction Open "cube"),
-                (Complex Take "cube" "key")]
+properActions = [(Interaction Open "cube")
+                ,(Complex Take "cube" "key")
+                ,(Complex Use "door" "key")]
 
 main :: IO()
 main = hspec $ do
@@ -111,3 +123,9 @@ main = hspec $ do
 
             it "Opens the cube" $ do
                 checkWorld ([Interaction Open "cube"]) (checkStatus "the test cube" Opened) `shouldBe` True
+
+            it "Tries to open the door while not having the key" $ do
+                checkWorld ([(properActions !! 1), (properActions !! 3)]) (checkStatus "A test door" Closed) `shouldBe` True
+
+            it "Open the cube, take the key and open the door" $ do
+                checkWorld (take 3 properActions) (checkStatus "A test door" Opened) `shouldBe` True
