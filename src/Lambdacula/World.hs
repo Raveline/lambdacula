0e3aed2a1795fcaecf0de7d2a31d5354529523aa
raveline <eraveline@gmail.com>
2013-05-29 18:10:18 +0200

    Removed tons of methods for ModelShortcuts.

--- src/Lambdacula/World.hs
+++ src/Lambdacula/World.hs
@@ -24,16 +24,11 @@ module Lambdacula.World
     ObjectStatus(..),
     roomByString,
     objectDescription,
-    singleAnswer,
     WorldAction,
     worldRooms,
     currentRoom,
-    changeStatus, 
-    changeRoom,
-    changeName,
-    pickItem,
-    openContainer,
-    pickItemFromContainer 
+    worldObjects,
+    objectAliases
 )
 where
 
@@ -47,9 +42,6 @@ import Control.Monad.State
 import Control.Applicative
 import Control.Lens hiding (Action)
 
-
-type Conversations = Map.Map String String
-type Interactions = Map.Map Action String
 type WorldAction = State World [String]
 type WorldSituation = State World ()
 type VertexToNodeInfo = Vertex -> (Room, String, [String])
@@ -176,10 +168,6 @@ instance Actionable RoomObject where
     actOn r = view objectReactions r r
     match s = (s `elem`) . ((:) <$> mainName <*> view objectAliases)
 
--- *****************
--- UTILITY FUNCTIONS
--- *****************
-
 -- Given a string and a room, try to find a RoomObject,
 -- an Exit or a Character matching the string. Send back
 -- a potential reaction function.
@@ -196,103 +184,3 @@ findObjectInteraction s ros = case newWorlds of
                 findAMatch = filter (match s) 
                 actOnAll :: (Actionable a) => [a] -> [Action -> Maybe String -> WorldAction] 
                 actOnAll = fmap actOn
-
--- Given a string, will return the World "as it is" and the string.
-singleAnswer :: String -> WorldAction 
-singleAnswer = return . (:[])
-
--- Change the status of an object
-changeStatus :: RoomObject          -- The room object to change
-                -> ObjectStatus     -- The new status
-                -> WorldSituation   -- Return a state World ()
-changeStatus ro st = do
-                        wos <- use worldObjects
-                        worldObjects .= rebuildList wos ro (ro & objectStatus.~ st)
-                        return ()
--- Change the room an object is stored in.
-changeRoom :: String
-            -> RoomObject        -- The room object to change
-            -> WorldSituation   -- Return a state World ()
-changeRoom name ro = do
-                        wos <- use worldObjects
-                        worldObjects .= rebuildList wos ro (ro { _inRoom = name })
-                        return ()
-
-changeName :: String
-            -> RoomObject
-            -> WorldSituation
-changeName name ro = do
-                        wos <- use worldObjects
-                        worldObjects .= rebuildList wos ro (ro { _ronames = (ObjectNames ["Test"])}) 
-                        return ()
-
--- Given a list of items, replace any version of an item by a new one
-rebuildList :: (Eq a) => [a]    -- A list
-                        -> a    -- The old element
-                        -> a    -- The new element
-                        -> [a]  -- A list with the element replaced
-rebuildList xs old new = case find (==old) xs of
-                            Just _  -> rebuildList' xs old new
-                            Nothing -> (new:xs)
-rebuildList' [] _ _ = []
-rebuildList' (x:xs) old new
-    | x == old = new:(rebuildList' xs old new)
-    | otherwise = x:(rebuildList' xs old new)
-
-
--- Called when a player wants to pick up an item.
--- We have to add the name of this item to the player's inventory.
--- And we have to remove this item from the room.
-pickItem :: RoomObject -> WorldAction
-pickItem ro = do
-                w <- get
-                inventory .= (mainName ro:(w^.inventory))
-                worldObjects .= filter(/= ro) (w & view worldObjects)
-                return ["You picked up " ++ (mainName ro)]
-
----------------------
--- Container related
----------------------
-
--- Object utilities
-isOpened :: RoomObject -> Bool
-isOpened ro = ro^.objectStatus == Opened
-
--- Given a room object, and a success string, open the container if possible
--- and display its content
-openContainer :: RoomObject -> String -> WorldAction
-openContainer ro sust
-    | isOpened ro = singleAnswer "It's already opened !"
-    | otherwise = do
-                    w <- get
-                    changeStatus ro Opened
-                    name <- use currentRoomName
-                    lookInsideContainer ro
-
-lookInsideContainer :: RoomObject -> WorldAction
-lookInsideContainer ro
-    | isOpened ro = singleAnswer $ (headName . _ronames $ ro) ++ " is closed, you can't look inside."
-    | otherwise = do 
-                    return ("It contains : ":displayContainerContent ro)
-    where
-        displayContainerContent ro = [mainName x| x <- (ro^.containedObjects)]
-
-pickItemFromContainer :: RoomObject         -- The container 
-                        -> String           -- The object to pick
-                        -> WorldAction
-pickItemFromContainer ro s
-    | s `elem` (map mainName $ _content . _rodetails $ ro) = do
-                                                let newRo = ro & containedObjects .~ (removeObjectFromList (ro^.containedObjects) s)
-                                                w <- get
-                                                worldObjects .= rebuildList (w^.worldObjects) ro newRo
-                                                singleAnswer $ "You picked up " ++ s
-    | otherwise = singleAnswer $ "There is no " ++  s ++ " in " ++ (mainName ro) ++ "."
-
--- Given a simple string, look for potential aliases in the list
-removeObjectFromList :: [RoomObject] -> String -> [RoomObject]
-removeObjectFromList ros s = case (findObjectToRemove s ros) of
-                                Just x -> filter (/= x) ros
-                                Nothing -> ros
-    where
-        findObjectToRemove :: String -> [RoomObject] -> Maybe RoomObject
-        findObjectToRemove s = find (elem s . view objectAliases)
