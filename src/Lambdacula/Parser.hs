730f296604b1dd8f1cfc45b39b45a3dddcd31d9e
raveline <eraveline@gmail.com>
2013-06-03 20:00:28 +0200

    Refactored. Less ugly than before (but not a beauty yet).

--- src/Lambdacula/Parser.hs
+++ src/Lambdacula/Parser.hs
@@ -5,7 +5,9 @@ module Lambdacula.Parser
 where
 
 import Data.List
+import Data.List.Split
 import Data.Char
+import Data.String
 import Lambdacula.Action
 
 
@@ -19,10 +21,16 @@ processInput str vs = buildAction verb objects
         statement = words . map (toLower) $ str
         
         buildAction :: Maybe Verb -> [String] -> PlayerAction
+        buildAction (Just v) [] = SimpleAction (actionType v) 
+        buildAction (Just v) [o] = Interaction (actionType v) o
+        buildAction (Just v) (o:o':[])
+                | checkForReversedVerb v  = Complex (actionType v) o' o
+                | otherwise = Complex (actionType v) o o'
         buildAction Nothing _ = SimpleAction Zilch
-        buildAction (Just v) [] = SimpleAction (getVerbAction v) 
-        buildAction (Just v) [o] = Interaction (getVerbAction v) o
-        buildAction (Just v) (o:o':[]) = Complex (getVerbAction v) o o'
+
+        checkForReversedVerb :: Verb -> Bool
+        checkForReversedVerb (Phrasal _ _ _ _ _ True) = True
+        checkForReversedVerb _ = False
 
 -- Given a list of verbs, try and find there is one in a given sentence
 findVerb :: [Verb] -> [String] -> Maybe Verb
@@ -34,49 +42,53 @@ findVerb (v:vs) s
         verbInSentence :: Verb -> [String] -> Bool
         verbInSentence (Transitive _ form prepositions _) s = form `elem` s
                                                     && prepositions `contains` s
-        verbInSentence (Phrasal _ form phrasal prepositions _) s = form `elem` s 
+        verbInSentence (Phrasal _ form phrasal prepositions _ _) s = form `elem` s 
                                                         && phrasal `elem` s 
                                                         && prepositions `contains` s
 
 contains [] _ = True
 contains xs ys = intersect xs ys /= []
 
--- Given a sentence without its first word (that would be the verb !), try and find
--- the objects in this sentence.
--- We're trying to find two potential objects, when there can be none, one or two.
--- "Give the apple to the cook" = ["the apple", "the cook"]
--- "Turn on the lights" = ["the lights"]
--- "Yawn" = []
-analyseObjects :: [String] -> [String] -> [String] -> [String]
-analyseObjects sentence fstWds sndWds = matcher sentence posFst posSnd
-    where 
-        matcher :: [String] -> Maybe Int -> Maybe Int -> [String]
-        matcher [] _ _ = []
-        matcher s Nothing Nothing = [concatAndNoArticles s]
-        matcher s Nothing (Just x) = [concatAndNoArticles . take x $ s] ++ [concatAndNoArticles . drop x $ s]
-        matcher s (Just 0) Nothing = [concatAndNoArticles . tail $ s]
-        matcher s (Just x) Nothing = [concatAndNoArticles . take x $ s]
-        matcher s (Just 0) (Just x) = [concatAndNoArticles . take (x-1) . tail $ s] ++ [concatAndNoArticles . drop (x+1) $ s]
-        matcher s (Just x) (Just y) = [concatAndNoArticles . init . take (x-1) $ s] ++ [concatAndNoArticles . drop (y+1) $ s]
-        
-        posFst = findPositionOf fstWds sentence
-        posSnd = findPositionOf sndWds sentence
-        
-        findPositionOf :: [String] -> [String] -> Maybe Int
-        findPositionOf prepos sentence = findIndex (`elem` prepos) sentence 
+analyseObjects :: [String] -- A sentence, without the initial verb
+                -> [String] -- The first possible separators
+                -> [String] -- A list of objects
+analyseObjects sentence separators =  filter (/= "") . map(trim) . splitOn "___" . unwords $ parse sentence separators
+    where
+        -- Parse a sentence, replace separators by "_"
+        parse :: [String] -> [String] -> [String]
+        parse [] _ = []
+        parse (w:ws) sep 
+            | w `elem` sep = "___":(parse ws sep)
+            | otherwise = w:(parse ws sep)
+        -- Remove if starts with separator
+        removeHead :: [String] -> [String]
+        removeHead [] = []
+        removeHead ("___":xs) = xs
+        removeHead xs = xs
+        -- Trim
+        trim :: String -> String
+        trim = trimRight . trimLeft
+            where
+                trimLeft :: String -> String
+                trimLeft (' ':ws) = trimLeft ws
+                trimLeft ws = ws
+                trimRight :: String -> String
+                trimRight ws
+                    | length ws == 0 = ws
+                    | (last ws) == ' ' = trimRight $ take ((length ws)- 1) ws
+                    | otherwise = ws
 
-        concatAndNoArticles = unwords . (flip (\\) articles)
 
 findObjects :: [String] -> Maybe Verb -> [String]
 findObjects _ Nothing = []
 findObjects sentence (Just (Transitive _ form prep comp)) =
-    analyseObjects sentence prep comp
-findObjects sentence (Just (Phrasal _ form phrasal prep comp)) =
-    analyseObjects sentence (phrasal:prep) comp
+    analyseObjects sentence (prep ++ comp)
+findObjects sentence (Just (Phrasal _ form phrasal prep comp _)) =
+    analyseObjects sentence (phrasal:prep ++ comp)
 
 transformIntoAction :: Maybe Verb -> Maybe String -> Maybe String -> PlayerAction
 transformIntoAction Nothing _ _ = SimpleAction Zilch
-transformIntoAction (Just v) Nothing _ = SimpleAction $ getVerbAction v
-transformIntoAction (Just v) (Just o) Nothing = Interaction (getVerbAction v) o
-transformIntoAction (Just v) (Just o) (Just o') = Complex (getVerbAction v) o o'
+transformIntoAction (Just v) Nothing _ = SimpleAction $ actionType v
+transformIntoAction (Just v) (Just o) Nothing = Interaction (actionType v) o
+transformIntoAction (Just v) (Just o) (Just o') = Complex (actionType v) o o'
 
