536df301ceb511ace3d4612db1d9dd8c41d56391
raveline <eraveline@gmail.com>
2013-06-03 20:02:12 +0200

    Added testing of reversed order of objects.

--- test/SpecAction.hs
+++ test/SpecAction.hs
@@ -7,34 +7,38 @@ import Lambdacula.Parser
 speak = Transitive Talk "speak" ["with", "to"] ["about"]
 talk = Transitive Talk "talk" ["with", "to"] ["about"]
 ask = Transitive Talk "ask" ["about"] []
-lookFor = Phrasal Search "look" "for" [] ["in", "with"]
+lookFor = Phrasal Search "look" "for" [] ["in", "with"] False
 examine = Transitive Examine "examine" [] ["with"]
 look = Transitive Examine "look" [] ["with"] 
-lookAt = Phrasal Examine "look" "at" [] ["with"]
+lookAt = Phrasal Examine "look" "at" [] ["with"] False
 analyze = Transitive Examine "analyze" [] []
 quit = Transitive QuitGame "quit" [] []
-verbs = [speak, talk, ask, lookFor, lookAt, examine, look, analyze, quit]
+pick = Phrasal Take "take" "out" [] ["of"] True
+verbs = [speak, talk, ask, lookFor, lookAt, examine, look, analyze, quit, pick]
 
 main :: IO()
 main = hspec $ do
     describe "processInput" $ do
             it "Finds the transitive verb Speak to in a sentence" $ do
-                processInput "Speak to the neighbour" verbs `shouldBe` Interaction Talk "neighbour"
+                processInput "Speak to the neighbour" verbs `shouldBe` Interaction Talk "the neighbour"
 
             it "Doesn't find a verb in a sentence" $ do
                 processInput "Behold the power of the specs" verbs `shouldBe` SimpleAction Zilch
 
             it "Finds the phrasal verb \"Look for\" in a sentence" $ do
-                processInput "Look for an answer to this riddle" verbs `shouldBe` Interaction Search "answer to this riddle"
+                processInput "Look for an answer to this riddle" verbs `shouldBe` Interaction Search "an answer to this riddle"
 
             it "Decode \"Examine the cube\" as an Interaction Examine cube" $ do
-                processInput "Examine the cube" verbs `shouldBe` Interaction Examine "cube"
+                processInput "Examine the cube" verbs `shouldBe` Interaction Examine "the cube"
 
-            it "Finds a complex action with two objets" $ do
-                processInput "Speak with the count about the meaning of life" verbs `shouldBe` Complex Talk "count" "meaning of life"
+            it "Finds a complex action with two objects" $ do
+                processInput "Speak with the count about the meaning of life" verbs `shouldBe` Complex Talk "the count" "the meaning of life"
+
+            it "Finds a complex action with two objects, with inverted order" $ do
+                processInput "Take the hammer out of the box" verbs `shouldBe` Complex Take "the box" "the hammer"
 
             it "Finds the SimpleAction Examine" $ do
                 processInput "Look" verbs `shouldBe` SimpleAction Examine
 
             it "Shows that Look can be a phrasal (look at) as well as a transitive verb." $ do
-                processInput "Look at the painting" verbs `shouldBe` Interaction Examine "painting"
+                processInput "Look at the painting" verbs `shouldBe` Interaction Examine "the painting"
