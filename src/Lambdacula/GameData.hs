a1677e900cfe084699d793281bcf6c5bacb22313
raveline <eraveline@gmail.com>
2013-06-04 15:24:25 +0200

    Just working on the game.

--- src/Lambdacula/GameData.hs
+++ src/Lambdacula/GameData.hs
@@ -49,15 +49,37 @@ makeExit :: String          -- Main name
             -> String       -- Destination
             -> ObjectStatus -- Opened / Closed ?
             -> RoomObject   -- An Exit
-makeExit name aliases inRoom description room status = Exit (ObjectNames (name:aliases)) inRoom (basicMove room) (RoomObjectDetails Nada description []) room 
-
+makeExit name aliases inRoom description room status = Exit (ObjectNames (name:aliases)) inRoom (basicMove room) (RoomObjectDetails status description []) room 
 
 ldRooms = [Room "In front of the castle" "You're standing in front of the castle of Lambdacula, in the heart of transylvania. It is standing at the top of a moutain, as any proper gothic castle should be. In front of you, to the south, the gates of the castle lead to the inner yard. I could describe the howling wind, the eerie atmosphere, the uncanny mist, the noise of flapping bats and other items from my Dictionnary Of Transylvanian Clichés, but I think you've got the idea. To the south, you'll find the gate of the castle, that you can cross to enter into an inner yard. On the east, a little path should lead you to safety or towards new adventures, but, come on, try to finish this one first." 
         , Room "The southern gate" "This is the inner yard of the castle. Obviously, count Lambdacula must have financial trouble, or his skills in household management are more than lacking. The place is a wreck, let's face it. There is an awful stench everywhere, rats are running through the place, the windows are dusty and frankly, UNESCO World Heritage Centre would be appalled by this place. There is a pile of junk next to the gates of the castle, and if you stay there too long, I'd say a pool of vomit close to it." 
         , Room "A muddy path" "You're walking a long a path that goes down the mountain of Lambda, where the castle of Lambdacula was built. You'd like to go along the path more and leave this dreadful place to go back to civilization but a strange feeling prevent you from... oh, who am I kidding ? You can't escape, you coward, you've got to face the count and murder him, because this is the goal of this sick, twisted, vampirophobic videogame. If you've got an issue with that, just type \"quit\" and be gone.\nWhere was I ? So, yes, the path could let you go away. The only interesting place here is a shack. It's most likely where the gamekeeper of Count Lambdacula used to live. Well, there is the slight possibility that the gamekeeper is immortal and still lives there, so you never know, that's the issue with undeads." 
-        , Room "The gamekeeper shack" "You're inside a simple shack made out of rudimentary logs. Everything is covered with spider webs. Apparently, no one has been there for a long time, though you could swear there are some footsteps on the dusty floorboards. On the wall opposite to the door, there is something that must have been a bed long ago. Next to it, a night table with an old copy of \"Lady's Chatterley's Lover\". There is also a cupboard, a diner table that was half-eaten by termites and a big bucket that must have been used for some very basic form of washing up. Decorum is not really the place forte, though there is a nice rug on the floor." ]
+        , Room "The gamekeeper shack" "You're inside a simple shack made out of rudimentary logs. Everything is covered with spider webs. Apparently, no one has been there for a long time, though you could swear there are some footsteps on the dusty floorboards. On the wall opposite to the door, there is something that must have been a bed long ago. Next to it, a night table. There is also a cupboard, a diner table that was half-eaten by termites and a big bucket that must have been used for some very basic form of washing up."]
 
 ldObjects = [makeExit "south" [] "In front of the castle" "the gate of the castle" "The southern gate" Opened
+            ,makeExit "east" [] "In front of the castle" "a path on the mountain" "A muddy path" Opened
             ,makeExit "north" [] "The southern gate" "the gate of the castle" "In front of the castle" Opened
             ,makeExit "west" [] "A muddy path" "an upward path to the castle" "In front of the castle" Opened
-            ,makeExit "door" [] "The gamekeeper shack" "a way out to the path below Lambdacula's Castle" "A muddy path" Opened]
+            ,makeExit "door" [] "A muddy path" "the gamekeeper shack" "The gamekeeper shack" Opened
+            ,makeExit "door" [] "The gamekeeper shack" "a way out to the path below Lambdacula's Castle" "A muddy path" Opened
+            ,simpleObject ["the book", "book", "Lady Chatterley", "Lady's Chatterley's Lover"] "The gamekeeper shack" ladyChatAction "On the night table, an old copy of Lady Chatterley's Lover."
+            ,simpleObject ["the rug", "rug", "oriental touch"] "The gamekeeper shack" rugAction "Decorum is not really the place forte, though there is a nice rug on the floor."]
+
+
+
+-------------------
+-- Ojbect methods
+-------------------
+-- Reminder:
+-- ROBehaviour = object -> action -> Maybe String for complex actions
+-- And return a WorldAction
+
+-- Lady Chaterley's Lover
+ladyChatAction :: RoomObjectBehaviour
+ladyChatAction book Use _ = singleAnswer $ "What do you want to use the book on ?"
+ladyChatAction book Examine _ = singleAnswer $ "Wow, it's a very old print. It looks like it's seen better day though. The previous owner seemed to love this book a lot. Perhaps a bit too much, actually."
+ladyChatAction book Take _ = pickItem book
+
+rugAction :: RoomObjectBehaviour
+rugAction rug Examine _ = singleAnswer $ "OK, it's probably not a masterwork as far as tapestry is concerned, but at least they were trying."
+
