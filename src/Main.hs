7c51484bab91fa4b0bc45cf5e574629ad5f53c31
raveline <eraveline@gmail.com>
2013-06-28 12:12:53 +0200

    Integration of Haskeline.

--- src/Main.hs
+++ src/Main.hs
@@ -14,9 +14,11 @@ import Control.Lens hiding(Action)
 import Lambdacula.Action
 import Lambdacula.ModelShortcuts
 
+import System.Console.Haskeline
+
 main = do
         printStrs $ displayRoom (view currentRoom aWorld) (view currentObjects aWorld)
-        promptLoop aWorld
+        runInputT defaultSettings (promptLoop aWorld)
 
 -- Load it from WorldBuilder
 aWorld = buildWorld ldRooms ldObjects
